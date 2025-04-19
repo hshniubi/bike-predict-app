f398322d764fa56e2cb8ab351110d0f816b764c7
hshniubi <hesihan88888@gmail.com>
2025-04-19 15:18:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,6 @@ input_df = pd.DataFrame([{
 
 # 预测
 if st.button("Predict"):
-    input_df = input_df.reindex(columns=model.feature_names_in_)
+    input_df = input_df.reindex(columns=model.feature_names_in_, fill_value=0)
     prediction = model.predict(input_df)[0]
     st.success(f"Predicted bike rental count: {int(prediction)}")
